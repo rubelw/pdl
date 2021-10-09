3080f84f40e8c6958a85d9317c5dcee80fa59a6d
rubelw <willrubel@gmail.com>
2021-10-09 15:16:41 -0500

    Bump version: 0.1.8 → 0.1.9

--- src/peopledatalabs/__init__.py
+++ src/peopledatalabs/__init__.py
@@ -6,7 +6,7 @@ __version__ = pkg_resources.get_distribution('peopledatalabs').version
 
 __all__= []
 __title__ = 'peopledatalabs'
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 __author__ = 'Will Rubel'
 __author_email__ = "willrubel@gmail.com"
 
