ccc4139d615f12ddf922fb4fc0bcbb71fc4128cc
rubelw <willrubel@gmail.com>
2021-10-09 15:26:08 -0500

    Bump version: 0.1.10 → 0.1.11

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup_requires = (
 
 setup(
     name='peopledatalabs',
-    version='0.1.10',
+    version='0.1.11',
     author='Will Rubel',
     author_email='willrubel@gmail.com',
     description=DESCRIPTION,
