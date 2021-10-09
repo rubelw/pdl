bfcd0e79ff88cffdb1545aea562d4213d2297105
rubelw <willrubel@gmail.com>
2021-10-08 21:06:33 -0500

    Testing bump version

--- src/peopledatalabs/__init__.py
+++ src/peopledatalabs/__init__.py
@@ -1,9 +1,10 @@
+from __future__ import absolute_import, division, print_function
 import pkg_resources
 from .pdl import *
 
 __version__ = pkg_resources.get_distribution('peopledatalabs').version
-__all__= []
 
+__all__= []
 __title__ = 'peopledatalabs'
 __version__ = '0.1.0'
 __author__ = 'Will Rubel'
