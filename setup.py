4ad7ebdf0ddbefbe5bd137fa3b647a652692e63f
rubelw <willrubel@gmail.com>
2021-10-09 20:49:29 -0500

    Bump version: 0.1.24 → 0.1.25

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup_requires = (
 
 setup(
     name='peopledatalabs',
-    version='0.1.24',
+    version='0.1.25',
     author='Will Rubel',
     author_email='willrubel@gmail.com',
     description=DESCRIPTION,
--- src/peopledatalabs/__init__.py
+++ src/peopledatalabs/__init__.py
@@ -6,7 +6,7 @@ __version__ = pkg_resources.get_distribution('peopledatalabs').version
 
 __all__= []
 __title__ = 'peopledatalabs'
-__version__ = '0.1.24'
+__version__ = '0.1.25'
 __author__ = 'Will Rubel'
 __author_email__ = "willrubel@gmail.com"
 
